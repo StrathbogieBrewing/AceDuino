712be06ecdec7a957a8a0973609b8f980bd6c550
John <john@solarhot.org>
2021-05-27 18:42:02 +1000

    changed to relative symbolic links

---
+++ clone.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+# readarray array <<< $( cat "$@" )
+# # readarray array <<< $( cat "repos.list" )
+# mkdir -p ~/git && cd ~/git
+#
+#
+#
+# #!/bin/bash
+# filename='company.txt'
+n=1
+while read line; do
+# reading each line
+cd ..
+echo "Line No. $n : $line"
+cd AceDuino
+
+
+n=$((n+1))
+done < "repos.list"
+
+# for element in ${array[@]}
+# do
+#   echo "clonning $element"
+#   # git clone $element
+# done
