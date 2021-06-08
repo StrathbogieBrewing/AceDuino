667fe6c4e84c3bce07d2c8a2766c531c7aeb3dce
Johny <johnyhoops@gmail.com>
2021-06-08 12:13:32 +1000

    Batch commit

--- push.sh
+++ push.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 find ../ -mindepth 1 -maxdepth 1 -type d -print -exec git -C {} add . \;
 find ../ -mindepth 1 -maxdepth 1 -type d -print -exec git -C {} commit -m "Batch commit" \;
-# find ../ -mindepth 1 -maxdepth 1 -type d -print -exec git -C {} push \;
+find ../ -mindepth 1 -maxdepth 1 -type d -print -exec git -C {} push \;
 
 
 # repos=($(cat repos.list))
